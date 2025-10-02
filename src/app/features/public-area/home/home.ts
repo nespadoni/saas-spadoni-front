a3815b31775921c8deeffd6de1dac7696a44905a
Neto Spadoni <137246816+nespadoni@users.noreply.github.com>
2025-10-02 14:43:00 -0400

    feat(home): import additional sections in home component

--- src/app/features/public-area/home/home.ts
+++ src/app/features/public-area/home/home.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import {ScrollReveal} from '../../../shared/directives/scroll-reveal/scroll-reveal';
+import {ProductsSection} from './product/product';
+import {ArtisanSection} from './artisan/artisan';
+import {StoreSection} from './store/store';
+import {Testimonial} from './testimonial/testimonial';
 
 @Component({
   selector: 'app-home',
   imports: [
-    ScrollReveal
+    ScrollReveal,
+    ProductsSection,
+    ArtisanSection,
+    StoreSection,
+    Testimonial
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
