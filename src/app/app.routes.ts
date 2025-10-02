0bc024db735633094c9e26c5793208fbad641e7f
Neto Spadoni <137246816+nespadoni@users.noreply.github.com>
2025-10-02 15:57:10 -0400

    feat(routes): add auth route with lazy loading for authentication module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import {Home} from './features/public-area/home/home';
 
 export const routes: Routes = [
   {
+    path: 'auth', // O segmento principal da URL
+    loadChildren: () => import('./features/auth/auth-routing-module').then(m => m.AuthRoutingModule)
+  },
+  {
     path: 'home',
     component: Home,
   },
@@ -15,5 +19,6 @@ export const routes: Routes = [
     path: '**',
     redirectTo: '/home',
     pathMatch: 'full'
-  }
+  },
+
 ];
