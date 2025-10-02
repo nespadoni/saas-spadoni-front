209c59bfc2e54648d0124bcfc36372adfd89cb6b
Neto Spadoni <137246816+nespadoni@users.noreply.github.com>
2025-10-02 15:57:55 -0400

    feat(register-client): add register client component with template, styling, and tests

---
+++ src/app/features/auth/register/register-client/register-client.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-client',
+  imports: [],
+  templateUrl: './register-client.html',
+  styleUrl: './register-client.scss'
+})
+export class RegisterClient {
+
+}
